7c0747f5ab7af3017e9fddcc77e1901399c36352
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2023-05-09 14:38:14 +0200

    initial commit

---
+++ src/analyzer/Main.java
@@ -0,0 +1,10 @@
+package analyzer;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File(
+                "/Users/jano/hyperskill/File Type Analyzer (Java)/File Type Analyzer (Java)/task/src/resources" + args[0]);
+    }
+}
