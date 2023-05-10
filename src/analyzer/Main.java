41baa1b37a37e747a7dc16107b825985770d7f75
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2023-05-10 08:11:34 +0200

    feat: create basic single pattern analyzer

--- src/analyzer/Main.java
+++ src/analyzer/Main.java
@@ -1,10 +1,45 @@
 package analyzer;
 
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
+
     public static void main(String[] args) {
-        File file = new File(
-                "/Users/jano/hyperskill/File Type Analyzer (Java)/File Type Analyzer (Java)/task/src/resources" + args[0]);
+        if (args.length < 3) {
+            System.out.println("Please provide all(3) arguments");
+            System.exit(0);
+        }
+        String path = args[0];
+        String pattern = args[1];
+        String fileType = args[2];
+        System.out.println(FileAnalyzer.isPatternPresent(path, pattern) ? fileType : "Unknown file type");
     }
 }
+
+class FileAnalyzer {
+
+    private final static int BUFFER_SIZE = 4096;
+    private static boolean isFound = false;
+
+    public static boolean isPatternPresent(String file, String pattern) {
+        try (InputStream inputStream = new FileInputStream(file)) {
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int bytesRead = -1;
+
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                String byteString = new String(buffer);
+                int index = byteString.indexOf(pattern);
+                if (index != -1) {
+                    isFound = true;
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return isFound;
+    }
+}
+
